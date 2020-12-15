66677f6a0d81b34cd363fea45870eb5ad23d85a2
CorteZzz137 <45366251+CorteZzz137@users.noreply.github.com>
2020-12-15 19:35:03 +0300

    Add files via upload

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -2,5 +2,16 @@
 #include "..//task5/task5.h"
 
 TEST(Test5, 1) {
-	EXPECT_EQ(2.f, sum(list<int>{ 1, 2, 3 }));
+	list<int> a = { 1, 2, 3, 2, 1, 4 };
+	list<int> ans = { 1, 2, 3, 4 };
+	unic(a);
+	auto i1 = a.begin();
+	auto i2 = ans.begin();
+	for (; i1 != a.end() && i2 != ans.end(); i1++) {
+		if ((*i1) != (*i2)) {
+			EXPECT_FALSE(false);
+		}
+		i2++;
+	}
+	EXPECT_TRUE(true);
 }
--- task5/task5.h
+++ task5/task5.h
@@ -4,13 +4,8 @@
 
 using namespace std;
 
-float sum(list<int>& a) {
-
-	int sum = 0;
-	for (auto i : a) {
-		sum += i;
-	}
-
-	return static_cast<float>(sum) / static_cast<float>(a.size());
-
+void unic(list<int>& a) {
+	a.unique();
 }
+
+
